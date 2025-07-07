7c32ce6b5d8c42d39b6861d9b7eda14d7a8db5a0
jaimiemontague <95182505+jaimiemontague@users.noreply.github.com>
2025-07-07 11:58:46 -0600

    Fixed top nav bar issues on Android, updated app logo to be centered, updated App name to remove the hyphen.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,13 @@
 import { Stack } from 'expo-router';
 import { AuthProvider } from '@/contexts/AuthContext';
 import AuthWrapper from '@/components/AuthWrapper';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
     <AuthProvider>
       <AuthWrapper>
+        <StatusBar style="dark" backgroundColor="#ffffff" translucent={false} />
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="login" />
           <Stack.Screen name="(tabs)" />
